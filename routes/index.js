520ac888f3fef7d891b968d3fd376765817af33b
clonn <clonncd@gmail.com>
2013-07-25 19:26:11 +0800

    add route and view for sender

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,34 @@
  * GET home page.
  */
 
+
+var nodemailer = require("nodemailer");
+var mailCfg = require("../config").mail;
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+   service: "Gmail",
+   auth: {
+       user: mailCfg.user,
+       pass: mailCfg.password
+   }
+});
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.send = function(req, res) {
+	smtpTransport.sendMail({
+	   from: "Arecord.us <nodejs@arecord.us>", // sender address
+	   to: "caesar chi <clonncd@gmail.com>", // comma separated list of receivers
+	   subject: "Hello ✔", // Subject line
+	   text: "Hello world ✔" // plaintext body
+	}, function(error, response){
+	   if(error){
+	       console.log(error);
+	   }else{
+	       console.log("Message sent: " + response.message);
+	       res.send(response.message);
+	   }
+	});
 };
